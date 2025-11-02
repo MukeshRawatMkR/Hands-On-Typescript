96b1af5980d65738fe66bbffc411c482b914b0b2
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-11-02 21:17:06 +0530

    1done

---
+++ 56.1 - 1. Introduction to TypeScript.ts
@@ -0,0 +1,20 @@
+/*
+TS doenst support dynamic typing.
+Primitives Types supported in TS:
+string, number, boolean, undefined, null, bigint, symbol
+
+Syntax: 
+var<varible-name>:DT = <value>; //type annotation or type signature.
+*/
+
+let id : number = 25;
+let firstName = "Mukesh";//still works, internally uses DT. but check out below line.
+// firstName = 5;//type 'number' is not assignable to type 'string'., happens with 'var' as well.
+console.log(id, firstName);
+
+
+
+//union of types : to store any DTs value, either-or
+let fullName : number | string = "Mukesh Rawat";//supports both value.
+fullName = 25;
+console.log(fullName);
